f816eec4b28c638119320489d1ccd8ec63f453e2
josepvazquezp <jose.vazquez@iteso.mx>
2022-11-19 14:46:50 -0600

    all_post

--- all_proyect/Backend/server.mjs
+++ all_proyect/Backend/server.mjs
@@ -86,7 +86,9 @@ let brandSchema = mongoose.Schema({
     categoria: {type: String,
                 required: true},
     apartado: {type: Array,
-               require: false}
+               require: false},
+    url: {type: String,
+          required: true}
 });
 
 let Brand = mongoose.model('brands', brandSchema);
@@ -127,7 +129,7 @@ let productsSchema = mongoose.Schema({
              required: true},
     descripcion: {type: String,
                   required: true},
-    image: {type: String,
+    imagen: {type: String,
             required: true},
     categoria: {type: String,
                 required: true},
@@ -146,32 +148,308 @@ app.post('/api/users', (req, res) => {
         correo: {$regex: req.body.correo}
     }, function (err, docs) {
         if(docs.length == 0) {
-            if(req.body.tipo == 'user') {
-                let newUser = {nombre: req.body.nombre, apellido: req.body.apellido, correo: req.body.correo, password: req.body.password, fecha: req.body.fecha, categorias: req.body.categorias, tipo: req.body.tipo};
-                let user = User(newUser);
-                user.save().then((doc) => console.log(chalk.green("Usuario creado: ") + doc));
+            let flag = true;
+            let message = "Hace falta los siguientes parametros: ";
+
+            if(req.body.nombre == undefined) {
+                flag = false;
+                message += "nombre";
             }
-            else if(req.body.tipo == 'marca') {
-                let newUser = {nombre: req.body.nombre, apellido: req.body.apellido, correo: req.body.correo, password: req.body.password, fecha: req.body.fecha, tipo: req.body.tipo, marca: req.body.marca, logo: req.body.logo, categoria: req.body.categoria};
-                let user = Brand(newUser);
-                user.save().then((doc) => console.log(chalk.green("Usuario creado: ") + doc));
+
+            if(req.body.apellido == undefined) {
+                if(flag) {
+                    message += "appellido";
+                }
+                else {
+                    message += ", appellido";
+                }
+
+                flag = false;
             }
-            else if(req.body.tipo == 'bazar'){
-                let newUser = {nombre: req.body.nombre, apellido: req.body.apellido, correo: req.body.correo, password: req.body.password, fecha: req.body.fecha, tipo: req.body.tipo, bazar: req.body.bazar, logo: req.body.logo, url: req.body.url};
-                let user = Bazaar(newUser);
-                user.save().then((doc) => console.log(chalk.green("Usuario creado: ") + doc));
+
+            if(req.body.correo == undefined) {
+                if(flag) {
+                    message += "correo";
+                }
+                else {
+                    message += ", correo";
+                }
+
+                flag = false;
+            }
+
+            if(req.body.password == undefined) {
+                if(flag) {
+                    message += "password";
+                }
+                else {
+                    message += ", password";
+                }
+
+                flag = false;
+            }
+
+            if(req.body.fecha == undefined) {
+                if(flag) {
+                    message += "fecha";
+                }
+                else {
+                    message += ", fecha";
+                }
+
+                flag = false;
+            }
+
+            if(req.body.tipo == undefined) {
+                if(flag) {
+                    message += "tipo";
+                }
+                else {
+                    message += ", tipo";
+                }
+
+                flag = false;
+            }
+
+            message += ".";
+
+            if(!flag) {
+                res.status(400);
+                res.send(message);
             }
             else {
-                res.sendStatus(401);
+                message = "Hace falta los siguientes parametros: ";
+
+                if(req.body.tipo == 'user') {
+                    if(req.body.categorias == undefined || req.body.categorias.length == 0) {
+                        flag = false;
+                        message += "categorias"
+                    }
+
+                    message += ".";
+
+                    if(!flag) {
+                        res.status(400);
+                        res.send(message);
+                    }
+                    else {
+                        let newUser = {nombre: req.body.nombre, apellido: req.body.apellido, correo: req.body.correo, password: req.body.password, fecha: req.body.fecha, categorias: req.body.categorias, tipo: req.body.tipo};
+                        let user = User(newUser);
+                        user.save().then((doc) => console.log(chalk.green("Usuario creado: ") + doc));
+                        res.sendStatus(201);
+                    }
+                }
+                else if(req.body.tipo == 'marca') {
+                    if(req.body.marca == undefined) {
+                        flag = false;
+                        message += "marca";
+                    }
+        
+                    if(req.body.logo == undefined) {
+                        if(flag) {
+                            message += "logo";
+                        }
+                        else {
+                            message += ", logo";
+                        }
+
+                        flag = false;
+                    }
+
+                    if(req.body.url == undefined) {
+                        if(flag) {
+                            message += "url";
+                        }
+                        else {
+                            message += ", url";
+                        }
+
+                        flag = false;
+                    }
+
+                    if(req.body.categoria == undefined) {
+                        if(flag) {
+                            message += "categoria";
+                        }
+                        else {
+                            message += ", categoria";
+                        }
+
+                        flag = false;
+                    }
+
+                    message += ".";
+
+                    if(!flag) {
+                        res.status(400);
+                        res.send(message);
+                    }
+                    else {
+                        let newUser = {nombre: req.body.nombre, apellido: req.body.apellido, correo: req.body.correo, password: req.body.password, fecha: req.body.fecha, tipo: req.body.tipo, marca: req.body.marca, logo: req.body.logo, categoria: req.body.categoria, url: req.body.url};
+                        let user = Brand(newUser);
+                        user.save().then((doc) => console.log(chalk.green("Usuario creado: ") + doc));
+                        res.sendStatus(201);
+                    }
+                }
+                else if(req.body.tipo == 'bazar') {
+                    if(req.body.bazar == undefined) {
+                        flag = false;
+                        message += "bazar";
+                    }
+        
+                    if(req.body.logo == undefined) {
+                        if(flag) {
+                            message += "logo";
+                        }
+                        else {
+                            message += ", logo";
+                        }
+
+                        flag = false;
+                    }
+
+                    if(req.body.url == undefined) {
+                        if(flag) {
+                            message += "url";
+                        }
+                        else {
+                            message += ", url";
+                        }
+
+                        flag = false;
+                    }
+
+                    message += ".";
+
+                    if(!flag) {
+                        res.status(400);
+                        res.send(message);
+                    }
+                    else {
+                        let newUser = {nombre: req.body.nombre, apellido: req.body.apellido, correo: req.body.correo, password: req.body.password, fecha: req.body.fecha, tipo: req.body.tipo, bazar: req.body.bazar, logo: req.body.logo, url: req.body.url};
+                        let user = Bazaar(newUser);
+                        user.save().then((doc) => console.log(chalk.green("Usuario creado: ") + doc));
+                        res.sendStatus(201);
+                    }
+                }
+                else {
+                    res.sendStatus(400);
+                }
             }
-            
         }
         else {
-            res.sendStatus(401);
+            res.status(401);
+            res.send("Ya existe un usuario con ese correo.");
         }
     });
 
     
+});
+
+app.post('/api/products/:email', (req, res) => {
+    let email = req.params.email;
+
+    Brand.find({
+        correo: {$regex: email}
+    }, function (err, docs) {
+        if(docs.length == 1) {
+            let marca = docs[0].marca;
+            let logo = docs[0].logo;
+
+            Product.find({        
+            }, function (err, docs) {
+                let temp = docs;
+
+                let local_id = temp.length > 0? temp[temp.length - 1].local_id + 1 : 1;
+
+                if(docs.length == 0) {
+                    let flag = true;
+                    let message = "Hace falta los siguientes parametros: ";
+        
+                    if(req.body.nombre == undefined) {
+                        flag = false;
+                        message += "nombre";
+                    }
+        
+                    if(req.body.precio == undefined) {
+                        if(flag) {
+                            message += "precio";
+                        }
+                        else {
+                            message += ", precio";
+                        }
+        
+                        flag = false;
+                    }
+        
+                    if(req.body.categoria == undefined) {
+                        if(flag) {
+                            message += "categoria";
+                        }
+                        else {
+                            message += ", categoria";
+                        }
+        
+                        flag = false;
+                    }
+        
+                    if(req.body.stock == undefined) {
+                        if(flag) {
+                            message += "stock";
+                        }
+                        else {
+                            message += ", stock";
+                        }
+        
+                        flag = false;
+                    }
+        
+                    if(req.body.descripcion == undefined) {
+                        if(flag) {
+                            message += "descripcion";
+                        }
+                        else {
+                            message += ", descripcion";
+                        }
+        
+                        flag = false;
+                    }
+        
+                    if(req.body.imagen == undefined) {
+                        if(flag) {
+                            message += "imagen";
+                        }
+                        else {
+                            message += ", imagen";
+                        }
+        
+                        flag = false;
+                    }
+        
+                    message += ".";
+        
+                    if(!flag) {
+                        res.status(400);
+                        res.send(message);
+                    }
+                    else {
+                        let newUser = {local_id: local_id, nombre: req.body.nombre, precio: req.body.precio, categoria: req.body.categoria, stock: req.body.stock, descripcion: req.body.descripcion, imagen: req.body.imagen, marca: marca, logo: logo};
+                        let user = Product(newUser);
+                        user.save().then((doc) => console.log(chalk.green("Producto creado: ") + doc));
+                        res.sendStatus(201);
+                    }
+                }
+                else {
+                    res.status(401);
+                    res.send("Ya existe ese producto.");
+                }
+            }); 
+        }
+        else {
+            res.status(400);
+            res.send("No se encontro la marca.");
+        }
+    });
 });
 
 app.get('/api/users/:email', (req, res) => {
@@ -179,6 +457,13 @@ app.get('/api/users/:email', (req, res) => {
     
     User.find({
         correo: {$regex: email}
+    }, function (err, docs) {
+        res.send(docs);
+    });
+});
+
+app.get('/api/products', (req, res) => {
+    Product.find({        
     }, function (err, docs) {
         res.send(docs);
     });
